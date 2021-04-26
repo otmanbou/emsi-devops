7966b884ea2b31edd88fde84076a6f7622b3f280
root <otmaneford@gmail.com>
2021-04-26 16:21:23 +0000

    task...

--- 0x02-web_server/0-transfer_file.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-if [ "$#" -eq 4 ]
-then
-    scp -i $4  $1 $3@$2:~/ 
-else 
-    echo "Usage: 0-transfer_file PATH_TO_FILE IP USERNAME PATH_TO_SSH_KEY"
-    exit 1
-fi    
--- 0x02-web_server/1-install_nginx_web_server.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-sudo apt-get install nginx -y 
-mkdir /var/www/myHolbertonPage/
-cd /var/www/myHolbertonPage/
-touch index.html
-echo "Welcome to Holberton School" > index.html
-sudo sed - i "s/\/var\/www\/html/\/var\/www\/myHolberonPage" /etc/nginx/sites-enabled/default
-sudo systemctl restart nginx 
